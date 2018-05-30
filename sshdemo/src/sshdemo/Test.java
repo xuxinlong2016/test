6c5c1765d5584b4a445fc5d776c5aeb14ffd2d33
xuxinlong <393251079@qq.com>
2018-05-30 17:52:41 +0800

    t
    t

---
+++ sshdemo/src/sshdemo/Test.java
@@ -0,0 +1,19 @@
+package sshdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+public static void main(String[] args) {
+	ArrayList<Integer> list = new ArrayList<Integer>();
+	list.add(5);
+	list.add(1);
+	list.add(4);
+	list.add(3);
+	list.add(1);
+	System.out.println(list.toString());
+}
+public static List sortList(ArrayList list) {
+	List resultList = new ArrayList();
+}
+}
